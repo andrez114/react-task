1b66f4b49937e65947dce59ad4bb2c71c09a7bf1
Oscar Millan <andrez114.am@gmail.com>
2023-02-11 17:45:31 -0600

    cambios de color

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -6,7 +6,7 @@ function TaskCard({ task }) {
 
 
     return (
-        <div className='bg-gray-800 text-white rounded-md'>
+        <div className='bg-gray-800 text-white ring-blue-500/50 '>
             <h1 className='text-xl font-bold capitalize'>{task.title}</h1>
             <p className='text-gray-500 text-sm'>{task.description}</p>
             <button className='bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400' onClick={() => {
