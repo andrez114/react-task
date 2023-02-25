0f4a65527739ac9b834ef20d9db2d0e5c792ad4d
Oscar Millan <andrez114.am@gmail.com>
2023-02-25 16:06:10 -0600

    cambio css

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,22 +1,25 @@
-import TaskCard from "./TaskCard"
-import {useContext} from 'react'
-import {TaskContext} from '../context/TaskContext'
+import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
 function TaskList() {
+  const { tasks } = useContext(TaskContext);
 
-    const {tasks} = useContext(TaskContext)
-
-    if (tasks.length === 0) {
-        return <h1 className="text-white text-4xl font-bold text-center">no hay tareas aún</h1>
-    }
-
+  if (tasks.length === 0) {
     return (
-        <div className="grid grid-cols-4 gap-2 p-4">
-            {tasks.map((task) => (
-                <TaskCard key={task.id} task={task}/>
-            ))}
-        </div>
-    )
+      <h1 className="text-white text-4xl font-bold text-center">
+        no hay tareas aún
+      </h1>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 p-4">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
 }
 
-export default TaskList
+export default TaskList;
